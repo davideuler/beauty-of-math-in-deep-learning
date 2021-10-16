d7fa2a63c3c280ce1b0a6eb11407b98027b70f5e
david ullua <davidullua@ihopeit.com>
2021-10-16 17:35:01 +0800

    refactored

--- predict.py
+++ predict.py
@@ -8,23 +8,32 @@ silence_tensorflow()
 #调用模型
 new_model = tf.keras.models.load_model('tf_model.h5')
 
-#调用模型进行预测识别
-filename = "digit-number-3.jpg"
-im = Image.open(filename)  #读取图片路径
-im = im.resize((28,28)) #调整大小和模型输入大小一致
-im = np.array(im)
+#调用模型对输入的图片进行识别，输出一个预测的数字
+def predict_digit(filename):
+    im = Image.open(filename)  #读取图片路径
+    im = im.resize((28,28)) #调整大小和模型输入大小一致
+    im = np.array(im)
 
-#对图片进行灰度化处理
-p3 = im.min(axis = -1)
-plt.imshow(p3,cmap = 'gray')
-plt.show()
+    #对图片进行灰度化处理
+    p3 = im.min(axis = -1)
+    plt.imshow(p3,cmap = 'gray')
+    plt.show()
 
-#将白底黑字变成黑底白字   由于训练模型是这种格式
-for i in range(28):
-   for j in range(28):
-       p3[i][j] = 255-p3[i][j]
+    #将白底黑字变成黑底白字   由于训练模型是这种格式
+    for i in range(28):
+        for j in range(28):
+            p3[i][j] = 255-p3[i][j]
 
-#模型输出结果是每个类别的概率，取最大的概率的类别就是预测的结果
-ret = new_model.predict((p3/255).reshape((1,28,28)))
-number = np.argmax(ret) 
-print("filename: %s predicted:%s" % (filename, number) )
+    #模型输出结果是每个类别的概率，取最大的概率的类别就是预测的结果
+    ret = new_model.predict((p3/255).reshape((1,28,28)))
+    number = np.argmax(ret) 
+    return number
+
+input_file = "digit-number-7.jpg"
+print("filename: %s predicted:%s" % ( input_file, predict_digit(input_file) ) )
+
+input_file = "digit-number-4.jpg"
+print("filename: %s predicted:%s" % ( input_file, predict_digit(input_file) ) )
+
+input_file = "digit-number-3.jpg"
+print("filename: %s predicted:%s" % ( input_file, predict_digit(input_file) ) )
--- train.py
+++ train.py
@@ -1,12 +1,15 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+
 import tensorflow as tf
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd
 
-mnist = tf.keras.datasets.mnist
 #载入 MNIST 数据集，并将整型转换为浮点型，除以 255 是为了归一化。
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 #使用 tf.keras.Sequential 建立模型，并且选择优化器和损失函数
@@ -18,13 +21,10 @@ model = tf.keras.models.Sequential([
 ])
 
 model.compile(optimizer='sgd',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
+              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 #训练模型
 history = model.fit(x_train, y_train, epochs=5,validation_data=(x_test,y_test))
-
 
 #模型评估
 model.evaluate(x_test,  y_test, verbose=2)
